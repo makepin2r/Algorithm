f538eba72ec184d699c5f7b40298db0c69d716f9
Seung Hyun Tark <makepin2r@gmail.com>
2023-06-20 10:36:04 +0900

    Update K번째수.js
    정리한 코드

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -2,9 +2,7 @@ function solution(array, commands) {
     var answer = [];
     
     for(let i = 0; i < commands.length; ++i){
-        let arr = array.slice((commands[i][0] - 1), commands[i][1]).sort((a,b) => a-b)[commands[i][2]-1];
-        console.log(arr)
-        answer.push(arr);
+        answer.push(array.slice((commands[i][0] - 1), commands[i][1]).sort((a,b) => a-b)[commands[i][2]-1]);
     }
     
     return answer;
