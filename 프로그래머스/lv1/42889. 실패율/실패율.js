6d7758795f8e2b233cf6b0b2d298f56d2f31c54c
Seung Hyun Tark <makepin2r@gmail.com>
2023-06-21 11:24:41 +0900

    실패율 불필요한 코드 제거, 주석 정리

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,19 +1,25 @@
 function solution(N, stages) {
-    let sortStage = [...stages].sort((a,b) => a-b);
     let users = stages.length; // 도전중인 플레이어 수 계산용
     let answer = new Map();
+    
+    // map 초기화
     for(let i = 1; i <= N; ++i){
-        answer.set(i, 0); // map 초기화
+        answer.set(i, 0); 
     }
+    
+    // 도전중인 플레이어 수 
     stages.forEach(v => {
-        // 마지막 스테이지 클리어한 데이터 제외
-        if(v <= N) answer.set(v, answer.get(v) + 1)
+        if(v <= N) // 마지막 스테이지 클리어한 데이터는 제외
+            answer.set(v, answer.get(v) + 1)
     })
 
+    // 실패율 계산
     for(let [k,v] of answer){
         let temp = v
-        answer.set(k, v/users) // 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+        answer.set(k, v/users) 
         users -= temp;
     }
+    
+    // 실패율 높은 순으로 내림차순 정렬
     return [...new Map([...answer.entries()].sort((a, b) => b[1] - a[1])).keys()];
 }
