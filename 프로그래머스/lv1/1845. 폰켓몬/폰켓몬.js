76454d18271898452c89a3de91eef52f395b4453
Seung Hyun Tark <makepin2r@gmail.com>
2023-06-21 15:46:25 +0900

    [level 1] Title: 폰켓몬, Time: 0.25 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,8 +1,3 @@
 function solution(nums) {
-    let numSet = new Set();
-    for(let i = 0; i < nums.length; ++i){
-        numSet.add(nums[i]);
-    }
-    
-    return (numSet.size < (nums.length / 2) ? numSet.size : nums.length / 2);
-}
+    let numSet = new Set(nums); 
+    return (numSet.size < (nums.length / 2) ? numSet.size : nums.length / 2);}
